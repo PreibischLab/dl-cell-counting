b7867536bb6fc35a3725bb416a5a030c2c5385b5
milkyklim <klim.kolyvanov@gmail.com>
2017-10-23 16:07:58 +0200

    useful bash script for renaming generated files

---
+++ algorithm/utils/bash-master.sh
@@ -0,0 +1,14 @@
+idx=1; 
+ext=".png"
+for file in cells*; 
+do 
+	fromi=$file # from cells
+	toi=$(printf "%03dcell$ext\n" "$idx"); # to cells  
+	froml="dots_"${file#*_}; # from labels 
+	tol=$(printf "%03ddots$ext\n" "$idx"); # to labels
+ 
+	mv $fromi $toi
+	mv $froml $tol
+
+	((idx++)); 
+done
